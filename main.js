541e0c655af228332bca8dc4e5348d9a0636f155
val_l <contacttovalerie@gmail.com>
2025-03-23 08:05:28 -0400

    Added save and reset buttons

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const addButton = document.querySelector(".add");
 const currentNumber = document.querySelector(".current-number");
 const subtractButton = document.querySelector(".subtract");
+const saveButton = document.querySelector(".save");
+const resetButton = document.querySelector(".reset");
+const savedNumbers = document.querySelector(".saved-numbers");
 
 let counter = 0;
 
@@ -20,11 +23,11 @@ const buttonCount = function (button, calculation) {
 
     currentNumber.innerText = counter;
 
-    if (counter >= 10 && counter < 20) {
+    if (counter < 0) {
       document.body.style.backgroundColor = "#F1E6FA";
-    } else if (counter >= 20 && counter < 30) {
+    } else if (counter >= 5 && counter < 10) {
       document.body.style.backgroundColor = "#E6C8F1";
-    } else if (counter >= 30 && counter < 40) {
+    } else if (counter >= 10 && counter < 20) {
       document.body.style.backgroundColor = "#D0A7E5";
     } else {
       document.body.style.backgroundColor = "#D8A7D8";
@@ -34,3 +37,11 @@ const buttonCount = function (button, calculation) {
 
 buttonCount(addButton, add);
 buttonCount(subtractButton, subtract);
+
+saveButton.addEventListener("click", function () {
+  savedNumbers.innerText += ` ${currentNumber.innerText}`;
+});
+
+resetButton.addEventListener("click", function () {
+  savedNumbers.innerText = ``;
+});
